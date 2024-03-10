906376775052104a269fa98f011f8e165c8676d7
Daniil Leshchev <leshev.da@gmail.com>
2024-03-10 08:20:43 +0200

    rendering all the products with map

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
 import products from '@/assets/data/products';
 
@@ -6,9 +6,14 @@ import ProductListItem from '@/src/components/ProductListItem';
 
 export default function MenuScreen() {
   return (
-    <View>
-      <ProductListItem product={products[0]}/>
-      <ProductListItem product={products[1]}/>
-    </View>
+    <ScrollView>
+      {/* <ProductListItem product={products[0]}/>
+      <ProductListItem product={products[1]}/> */}
+      {
+        products.map((item) => {
+          return <ProductListItem product={item}/>
+        })
+      }
+    </ScrollView>
   )
 }
