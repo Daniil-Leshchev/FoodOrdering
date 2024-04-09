8ddba7ebc2513b3796659d0454dfef772624f3f8
Daniil Leshchev <leshev.da@gmail.com>
2024-04-09 11:18:57 +0500

    new order subscription

--- src/app/(admin)/orders/list/index.tsx
+++ src/app/(admin)/orders/list/index.tsx
@@ -1,11 +1,16 @@
 import { ActivityIndicator, FlatList, Text } from 'react-native';
 
 import OrderListItem from '@/src/components/OrderListItem';
-import React from 'react';
-import { useAdminOrderList } from '@/src/api/orders';
+import React, { useEffect } from 'react';
+import { useAdminOrderList, useInsertOrder } from '@/src/api/orders';
+import { supabase } from '@/src/lib/supabase';
+import { useQueryClient } from '@tanstack/react-query';
+import { useInsertOrderSubscription } from '@/src/api/orders/subscriptions';
 
 export default function OrdersScreen() {
   const { data: orders, isLoading, error} = useAdminOrderList({archived: false});
+
+  useInsertOrderSubscription();
 
   if (isLoading) {
     return <ActivityIndicator/>
