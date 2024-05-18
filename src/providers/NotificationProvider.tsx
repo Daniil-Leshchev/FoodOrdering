c297dcfe5ef111081bcb7c6ba01d7a0412d4f164
Daniil Leshchev <leshev.da@gmail.com>
2024-05-18 16:57:24 +0500

    setting expo push token to user profile

--- src/providers/NotificationProvider.tsx
+++ src/providers/NotificationProvider.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef, useState } from "react";
 import { PropsWithChildren } from "react";
 import { registerForPushNotificationsAsync } from "@/src/lib/notifications";
 import * as Notifications from 'expo-notifications';
+import { supabase } from "@/src/lib/supabase";
+import { useAuth } from "./AuthProvider";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -12,15 +14,27 @@ Notifications.setNotificationHandler({
 });
 
 const NotificationProvider = ({children} : PropsWithChildren) => {
-  const [expoPushToken, setExpoPushToken] = useState('');
+  const { profile } = useAuth();
+  const [expoPushToken, setExpoPushToken] = useState<string | undefined>('');
   const [notification, setNotification] = useState<Notifications.Notification | undefined>(undefined);
   const notificationListener = useRef<Notifications.Subscription>();
   const responseListener = useRef<Notifications.Subscription>();
 
+  const savePushToken = async (newToken: string | undefined) => {
+    setExpoPushToken(newToken);
+    if (!newToken) {
+      return;
+    }
+    //updating token in db
+    await supabase
+      .from('profiles')
+      .update({ expo_push_token: newToken })
+      .eq('id', profile.id);
+  };
+
   useEffect(() => {
     registerForPushNotificationsAsync()
-      .then((token) => setExpoPushToken(token ?? ''))
-      .catch((error: any) => setExpoPushToken(error));
+      .then((token) => savePushToken(token));
 
     notificationListener.current =
     Notifications.addNotificationReceivedListener((notification) => {
@@ -32,7 +46,6 @@ const NotificationProvider = ({children} : PropsWithChildren) => {
         console.log(response);
     });
   
-    console.log(expoPushToken);
     
     return () => {
       notificationListener.current &&
